5e6b9d9fa25c482bb9f95e46c3cad8dff88b9cce
anna.somik <kduzkdkuzkdkd@gmail.com>
2022-10-15 23:41:48 +0600

    CT-3 Added users auth logic

---
+++ backend/app/users.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+router.post('/',async (req, res) => {
+    const {username, password} = req.body;
+
+    if (!username || !password) {
+        return res.status(400).send({error: 'Data not valid'});
+    }
+
+    const userData = {
+        username,
+        password
+    };
+
+    try {
+        const user = new User(userData);
+        user.generateToken();
+        await user.save();
+        res.send(user);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+router.post('/sessions', async (req, res) => {
+    const user = await User.findOne({username: req.body.username});
+
+    if (!user) {
+        return res.status(401).send({message: 'Credentials are wrong!'});
+    }
+
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if (!isMatch) {
+        res.status(401).send({message: 'Credentials are wrong!'});
+    }
+
+    user.generateToken();
+    await user.save({validateBeforeSave: false});
+    res.send({message: 'Username and password correct', user});
+});
+
+
+router.delete('/sessions', async (req, res) => {
+    const token = req.get('Authorization');
+    const success = {message: 'Success'};
+
+    if (!token) return res.send(success);
+
+    const user = await User.findOne({token});
+
+    if (!user) return res.send(success);
+
+    user.generateToken();
+    await user.save({validateBeforeSave: false});
+
+    return res.send({success, user});
+});
+
+
+module.exports = router;
---
+++ backend/models/User.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const {nanoid} = require("nanoid");
+
+const Schema = mongoose.Schema;
+const SALT_WORK_FACTOR = 10;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: async value => {
+                const user = await User.findOne({username: value});
+
+                if (user) return false;
+            },
+            message: 'This user is already registered',
+        }
+    },
+    password:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    token:{
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true,
+        default: 'user',
+        enum: ['user', 'moderator'],
+    }
+});
+
+UserSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return  next();
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    const hash = await bcrypt.hash(this.password,salt);
+    this.password = hash;
+    next();
+});
+
+UserSchema.set('toJSON',{
+    transform:(doc, ret, options) => {
+        delete ret.password;
+        return ret;
+    }
+});
+
+UserSchema.methods.checkPassword = function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateToken = function () {
+    this.token = nanoid();
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
